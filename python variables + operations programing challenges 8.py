59ee0832f63a04e9b09ac36a1c4da591ed3e24af
Samuel Stewart <S.Stewart@albynschool.org>
2025-06-23 09:57:52 +0100

    hi

--- python variables + operations programing challenges 8.py
+++ python variables + operations programing challenges 8.py
@@ -9,3 +9,14 @@
 overall_goals_scored = 6
 overall_goals_conceded = 5
 number_of_games_played = 3
+
+goal_difference = (overall_goals_scored - overall_goals_conceded)
+if number_of_games_played == 10:
+    average_goals = overall_goals_scored / number_of_games_played
+    print("average goals per game", average_goals)
+    
+
+print("overall goals scored =",overall_goals_scored)
+print("overall goals conceded =",overall_goals_conceded)
+print("number of games played =",number_of_games_played)
+print("goal difference =",goal_difference)
