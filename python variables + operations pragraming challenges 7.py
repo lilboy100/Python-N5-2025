688e6868772db5cf22050c850fb1bd58059a0701
Samuel Stewart <S.Stewart@albynschool.org>
2025-06-23 09:28:58 +0100

    hi

--- python variables + operations pragraming challenges 7.py
+++ python variables + operations pragraming challenges 7.py
@@ -6,10 +6,11 @@
 # If the fine exceeds £5, add an additional £2 processing fee
 # Calculate and display the total fine
 
-days_overdue = 5
+days_overdue = 7.00
 fine_per_day = 0.50 
 proccesing_fee = 2.00
 
 total_fine = (days_overdue * fine_per_day)
-
+if total_fine > 5:
+    total_fine = total_fine + 2
 print(total_fine)
---
+++ python variables + operations programing challenges 8.py
@@ -0,0 +1,11 @@
+#Task 8: Sports Statistics
+#Create a program for a football team that:
+
+#Stores overall goals scored, overall goals conceded and number of games played
+#Calculates goal difference
+#If they played 10 games, calculate the average goals per game
+#Display all relevant statistics
+
+overall_goals_scored = 6
+overall_goals_conceded = 5
+number_of_games_played = 3
